26ebd8b7d0746f8cfd6b97a241993b01fbd07496
MatthewMunks <matthewmunks@gmail.com>
2022-11-08 16:37:04 +0000

    Beginning to migrate to Python3.

--- part_seg/evaluate.py
+++ part_seg/evaluate.py
@@ -60,9 +60,9 @@ def evaluate():
         with tf.device('/gpu:'+str(GPU_INDEX)):
             pointclouds_pl, labels_pl = MODEL.placeholder_inputs(BATCH_SIZE, NUM_POINT)
             is_training_pl = tf.placeholder(tf.bool, shape=())
-            print is_training_pl
+            print(is_training_pl)
             
-            print "--- Get model and loss"
+            print("--- Get model and loss")
             pred, end_points = MODEL.get_model(pointclouds_pl, is_training_pl)
             loss = MODEL.get_loss(pred, labels_pl)
             saver = tf.train.Saver()
